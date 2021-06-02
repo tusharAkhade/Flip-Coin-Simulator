644e69e82aad5fc75cabf76ad236ed1e09cf8ced
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-02 23:49:29 +0530

    Added: Flipping Coin multiple times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,16 @@
-random=$((RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+read -p "How many times you want to toss the coin: " num
+headCount=0
+tailCount=0
+for(( i=1; i<=$num; i++ ))
+do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		((headCount++))
+	else
+		((tailCount++))
+	fi
+done
+
+echo "Head won $headCount times"
+echo "Tail won $tailCount times"
